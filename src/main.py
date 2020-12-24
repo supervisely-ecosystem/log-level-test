cd9a779a743e70e901d258a4bc1a4f00be1411c9
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-12-24 08:52:36 +0200

    1 hour sleep

--- src/main.py
+++ src/main.py
@@ -2,12 +2,11 @@ import time
 import os
 import supervisely_lib as sly
 
+
 if __name__ == "__main__":
     print(f"LOGLEVEL={os.environ.get('LOGLEVEL', 'NOT DEFINED')}")
     print(f"logger.level ={sly.logger.level}")
 
-    #time.sleep(99999)
-    
     print("trace logs...")
     for i in range(5):
         time.sleep(0.3)
@@ -25,3 +24,5 @@ if __name__ == "__main__":
         time.sleep(0.3)
         sly.logger.info(f"warn {i}")
     sly.logger.critical("crucial")
+
+    time.sleep(60 * 60) # sleep one hour
